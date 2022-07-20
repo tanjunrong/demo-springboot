444bf8fefeac7fd689d4883ee9f3782cd5b968a3
tanjr <tanjunrong@github.com>
2022-07-21 02:24:42 +0800

    swagger->openapi

--- src/main/java/com/tanjr/demo/controller/demo/vo/DemoVO.java
+++ src/main/java/com/tanjr/demo/controller/demo/vo/DemoVO.java
@@ -1,6 +1,5 @@
 package com.tanjr.demo.controller.demo.vo;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -14,13 +13,13 @@ public class DemoVO {
 
     @Null(message = "id必须为空", groups = CreateGroup.class)
     @NotNull(message = "id不能为空",groups = GetGroup.class)
-    @ApiModelProperty(value = "demoId")
+//    @ApiModelProperty(value = "demoId")
     private Integer demoId;
 
     @NotNull(message = "id不能为空",groups = GetGroup.class)
-    @ApiModelProperty(value = "demo名称")
+//    @ApiModelProperty(value = "demo名称")
     private String name;
 
-    @ApiModelProperty(value = "demo描述")
+//    @ApiModelProperty(value = "demo描述")
     private String description;
 }
